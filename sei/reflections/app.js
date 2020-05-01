509bd2cd8bb102393c4bf75fa6f14947d291d170
Chuck Wagon <chuckwagon@Chucks-MacBook-Air.local>
2020-05-01 10:31:49 -0700

    there is a for loop

---
+++ sei/reflections/app.js
@@ -0,0 +1,9 @@
+let people = ["Albert","Anna","Ben","Carmen Sandiego","Dora","Eric","Francis","Gary","Helen","Ira","Jake","Jane","John","kate","Louis","Mike","Nancy","Oscar","Paula","QuIncy","Regina","Sabrina","trevor","Unice","Victoria","Winston","Waldo","Xavier","Yancy","Zack"]
+
+const findWaldo = (people) => {
+    console.log('findWaldo')
+    for (let i=0; i < people.length; i++) {
+    }
+    return ('Name is not found')
+}
+findWaldo()
